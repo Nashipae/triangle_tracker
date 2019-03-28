6d489b68a4ab4e4361318f2527815dcb5c528025
nwariara <nwariara@safaricom.co.ke>
2019-03-28 19:07:45 +0300

    update the index.js file with algorithm for scalene triangle

--- js/index.js
+++ js/index.js
@@ -17,3 +17,11 @@ if((userInput1 === userInput2 && userInput3 !== userInput1 && userInput3!==userI
 else{alert("Your triangle is NOT an isosceles triangle");}
 
 
+// For a scalene triangle, none of the sides is equal. However, the sum of any two sides of the triangle must be greater than the third side.
+
+var userInput1= parseInt(prompt("Please enter the 1st length of your triangle."));
+var userInput2= parseInt(prompt("Please enter the 2nd length of your triangle."));
+var userInput3= parseInt(prompt("Please enter the 3rd length of your triangle."));
+if((userInput1!==userInput2 && userInput2!==userInput3 && userInput1!==userInput3) && (userInput1+userInput2 >userInput3 || userInput1+userInput3 >userInput2 || userInput3+userInput2 >userInput1)&&(userInput1+userInput2 >userInput3 && userInput1+userInput3 >userInput2 && userInput3+userInput2 >userInput1)){alert("Your triangle is a scalene triangle");
+}
+else{alert("Your triangle is NOT a scalene triangle");}
