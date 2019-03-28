c79b48d6829060b80e0ec5ecb3b21ff63edbca48
nwariara <nwariara@safaricom.co.ke>
2019-03-28 17:20:24 +0300

    update the js file with isosceles triangle algorithm.

--- js/index.js
+++ js/index.js
@@ -5,4 +5,13 @@ var userInput2= parseInt(prompt("Please enter the 2nd length of your triangle.")
 var userInput3= parseInt(prompt("Please enter the 3rd length of your triangle."));
 //if(userInput1===userInput2 && userInput2===userInput3 && userInput1===userInput3){alert("Your triangle is an equilateral triangle")
 }
-else{alert("Your triangle is not an equilateral tr");}
+// else{alert("Your triangle is not an equilateral tr");}
+
+// For an isosceles triangle, two sides are exactly equal.
+
+var userInput1= parseInt(prompt("Please enter the 1st length of your triangle."));
+var userInput2= parseInt(prompt("Please enter the 2nd length of your triangle."));
+var userInput3= parseInt(prompt("Please enter the 3rd length of your triangle."));
+if((userInput1 === userInput2 && userInput3 !== userInput1 && userInput3!==userInput2)||(userInput2 === userInput3 && userInput1 !== userInput2 && userInput1!==userInput3)||(userInput1 === userInput3 && userInput2 !== userInput1 && userInput2!==userInput3)){alert("Your triangle is an isoscecles triangle");
+}
+else{alert("Your triangle is NOT an isosceles triangle");}
