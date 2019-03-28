c0e64a43b5ff4dd4473fa31c48f97d7b73b68998
nwariara <nwariara@safaricom.co.ke>
2019-03-28 17:02:22 +0300

    update overview file and js file

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//Equilateral Triangle Evaluation (For an equilateral triangle, all sides must be equal.)
+var equilateral=[];
+var userInput1= parseInt(prompt("Please enter the 1st length of your triangle."));
+var userInput2= parseInt(prompt("Please enter the 2nd length of your triangle."));
+var userInput3= parseInt(prompt("Please enter the 3rd length of your triangle."));
+//if(userInput1===userInput2 && userInput2===userInput3 && userInput1===userInput3){alert("Your triangle is an equilateral triangle")
+}
+//else{alert("Your triangle is not an equilateral tr");}
