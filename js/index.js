3a2a50eb220d665986ce3d890c561d5002a7e6ec
nwariara <nwariara@safaricom.co.ke>
2019-03-28 17:10:52 +0300

    update equilateral triangle algorithm.

--- js/index.js
+++ js/index.js
@@ -5,4 +5,4 @@ var userInput2= parseInt(prompt("Please enter the 2nd length of your triangle.")
 var userInput3= parseInt(prompt("Please enter the 3rd length of your triangle."));
 //if(userInput1===userInput2 && userInput2===userInput3 && userInput1===userInput3){alert("Your triangle is an equilateral triangle")
 }
-//else{alert("Your triangle is not an equilateral tr");}
+else{alert("Your triangle is not an equilateral tr");}
